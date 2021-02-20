f3ea75a22a8120d27217716b0ba834cc57af5ede
Fabricio Souza <fabricio.sistemas19@gmail.com>
2021-02-20 18:37:54 -0300

    Projeto Iniciado

---
+++ src/test/java/souza/fabricio/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package souza.fabricio.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
