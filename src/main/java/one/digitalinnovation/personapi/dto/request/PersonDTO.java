ced97c6d48a33626c0282942b5043d2b09f05452
Fabricio Souza <fabricio.sistemas19@gmail.com>
2021-02-23 21:12:33 -0300

    Created new database attributes

---
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -0,0 +1,58 @@
+package one.digitalinnovation.personapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotEmpty
+    @CPF
+    private String cpf;
+
+    private String birthDate;
+
+    //@NotEmpty
+    private String street;
+
+    //@NotEmpty
+    private String number;
+
+    //@NotEmpty
+    private String city;
+
+    //@NotEmpty
+    private String state;
+
+    //@NotEmpty
+    private String zipcode;
+
+    @Valid
+    @NotEmpty
+    private List<PhoneDTO> phones;
+
+    //@Valid
+    //@NotEmpty
+    //private List<AddressDTO> addresses;
+}
---
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,61 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    //@Column(nullable = false)
+    private String street;
+
+    //@Column(nullable = false)
+    private String number;
+
+    //@Column(nullable = false)
+    private String city;
+
+    //@Column(nullable = false)
+    private String state;
+
+    //@Column(nullable = false)
+    private String zipcode;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+
+    //@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    //private List<Address> addresses;
+}
